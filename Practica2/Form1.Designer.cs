7a94a7706872f0e773234a0332c8b2227cd66aed
RodrigoQAC <RodrigoQAC@DISPOSITIVO>
2025-03-18 08:04:15 -0300

    Agregar archivos de proyecto.

---
+++ Practica2/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace Practica2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEj1 = new System.Windows.Forms.Button();
+            this.btnEj2 = new System.Windows.Forms.Button();
+            this.btnEj3 = new System.Windows.Forms.Button();
+            this.lblInt = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnEj1
+            // 
+            this.btnEj1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnEj1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEj1.Location = new System.Drawing.Point(59, 58);
+            this.btnEj1.Name = "btnEj1";
+            this.btnEj1.Size = new System.Drawing.Size(190, 59);
+            this.btnEj1.TabIndex = 0;
+            this.btnEj1.Text = "EJERCICIO 1";
+            this.btnEj1.UseVisualStyleBackColor = true;
+            this.btnEj1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnEj2
+            // 
+            this.btnEj2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnEj2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEj2.Location = new System.Drawing.Point(295, 58);
+            this.btnEj2.Name = "btnEj2";
+            this.btnEj2.Size = new System.Drawing.Size(190, 59);
+            this.btnEj2.TabIndex = 1;
+            this.btnEj2.Text = "EJERCICIO 2";
+            this.btnEj2.UseVisualStyleBackColor = true;
+            // 
+            // btnEj3
+            // 
+            this.btnEj3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnEj3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEj3.Location = new System.Drawing.Point(526, 58);
+            this.btnEj3.Name = "btnEj3";
+            this.btnEj3.Size = new System.Drawing.Size(190, 59);
+            this.btnEj3.TabIndex = 2;
+            this.btnEj3.Text = "EJERCICIO 3";
+            this.btnEj3.UseVisualStyleBackColor = true;
+            // 
+            // lblInt
+            // 
+            this.lblInt.AutoSize = true;
+            this.lblInt.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInt.Location = new System.Drawing.Point(56, 188);
+            this.lblInt.Name = "lblInt";
+            this.lblInt.Size = new System.Drawing.Size(203, 29);
+            this.lblInt.TabIndex = 3;
+            this.lblInt.Text = "INTEGRANTES:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(290, 214);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(224, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Cuello Saavedra Franco";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(290, 258);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(153, 25);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Martinez Tomas";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(290, 294);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(140, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Meren Rodrigo";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(290, 331);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(146, 25);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Poliserpi Pablo ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(290, 369);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(115, 25);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Integarnte 5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(290, 403);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(115, 25);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Integrante 6";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblInt);
+            this.Controls.Add(this.btnEj3);
+            this.Controls.Add(this.btnEj2);
+            this.Controls.Add(this.btnEj1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEj1;
+        private System.Windows.Forms.Button btnEj2;
+        private System.Windows.Forms.Button btnEj3;
+        private System.Windows.Forms.Label lblInt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
